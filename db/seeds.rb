cf183ded0c0bb34c51edf2b9aed981d71e66fc90
Gareth Tan <garethwtan@gmail.com>
2013-08-04 07:03:29 -0400

    Change initial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,22 +12,7 @@ Song.create(title:"Seattle Yodel", artist:"Gorillaz")
 Song.create(title:"No Leaves, No Shade", artist:"Friendly Savages")
 Song.create(title:"Au Revoir", artist:"Flight Of The Conchords")
 Song.create(title:"Wow", artist:"Garfunkel and Oates")
-
 Song.create(title:"velocity girl", artist:"primal scream")
 Song.create(artist:"jonathan richman", title:"let her go into the darkness")
 Song.create(title:"silent night", artist:"sufjan stevens")
 Song.create(title:"belly button", artist:"sufjan stevens")
-
-
-
-10.times do |i|
-  User.create([email: "#{i}@abc.com", password: "12345678"])
-end
-
-10.times do |i|
-  UserUploaded.create(user_id: "#{i}", song_id: "#{i}")
-end
-
-10.times do |i|
-  UserHistory.create(user_id: "#{10 - i}", song_id: "#{i}")
-end
